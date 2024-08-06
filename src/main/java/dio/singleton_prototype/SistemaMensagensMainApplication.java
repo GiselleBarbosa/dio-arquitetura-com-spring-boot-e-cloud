bcbfb6dde2496c9681b9276f5a8a313b5339b455
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-06 10:51:26 -0300

    conceitos - Singleton ou Prototype

---
+++ src/main/java/dio/singleton_prototype/SistemaMensagensMainApplication.java
@@ -0,0 +1,24 @@
+package dio.singleton_prototype;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SistemaMensagensMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaMensagensMainApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(SistemaMensagem sistema) throws Exception {
+        return args -> {
+            sistema.enviarConfirmacaoCadastro();
+            sistema.enviarMensagemBoasVindas();
+            sistema.enviarConfirmacaoCadastro();
+        };
+    }
+
+}
