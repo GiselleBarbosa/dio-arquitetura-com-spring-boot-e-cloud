d86cc68e5a41811135350146c68a60c935518c28
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-06 11:12:42 -0300

    conceitos - application properties e values

---
+++ src/main/java/dio/properties_values/SpringPrimeirosPassosApplication.java
@@ -0,0 +1,12 @@
+package dio.properties_values;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrimeirosPassosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrimeirosPassosApplication.class, args);
+    }
+}
